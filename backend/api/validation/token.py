e57a1ff3a816ee92acd4cb3f5f486ca3337ed468
Frantisek Ember <f@ember.sk>
2024-12-21 16:18:34 +0100

    Use pydantic example values properly
    Pydantic model default values where misused as example values. This
    was obviously wrong.

--- backend/api/validation/token.py
+++ backend/api/validation/token.py
@@ -2,8 +2,28 @@ from pydantic import BaseModel
 
 
 class FakeTokenRequest(BaseModel):
-    email: str = "john1@example.com"
+    email: str
+
+    model_config = {
+        "json_schema_extra": {
+            "examples": [
+                {
+                    "email": "john1@example.com",
+                }
+            ]
+        }
+    }
 
 
 class FakeTokenResponse(BaseModel):
     token: str
+
+    model_config = {
+        "json_schema_extra": {
+            "examples": [
+                {
+                    "token": "SOMEJWTTOKEN",
+                }
+            ]
+        }
+    }
--- backend/api/validation/users.py
+++ backend/api/validation/users.py
@@ -5,3 +5,15 @@ class UserResponse(BaseModel):
     email: str = "john1@example.com"
     first_name: str = "John1"
     surname: str = "Doe"
+
+    model_config = {
+        "json_schema_extra": {
+            "examples": [
+                {
+                    "email": "john1@example.com",
+                    "first_name": "John1",
+                    "surname": "Doe",
+                }
+            ]
+        }
+    }
